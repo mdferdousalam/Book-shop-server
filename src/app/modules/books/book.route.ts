dbfc48195e3ca1ef97aeb0b4de76cf25e7f18a9b
mdferdousalam <mdferdousalam1989@yahoo.com>
2023-07-27 10:50:43 +0600

    book route updated

--- src/app/modules/books/book.route.ts
+++ src/app/modules/books/book.route.ts
@@ -1,11 +1,17 @@
 import express from 'express'
-import { createBookHandler, deleteBookHandler, getAllBooksHandler, getBookByIdHandler, updateBookHandler } from './book.controller'
+import {
+  createBookHandler,
+  deleteBookHandler,
+  getAllBooksHandler,
+  getBookByIdHandler,
+  updateBookHandler,
+} from './book.controller'
 const router = express.Router()
 
-router.get('/all-books', getAllBooksHandler)
-router.get('/single-book/:id', getBookByIdHandler)
-router.post('/add-book', createBookHandler)
-router.patch('/edit-book/:id', updateBookHandler)
-router.delete('/delete-book/:id', deleteBookHandler)
+router.get('/', getAllBooksHandler)
+router.get('/:id', getBookByIdHandler)
+router.post('/', createBookHandler)
+router.patch('/:id', updateBookHandler)
+router.delete('/:id', deleteBookHandler)
 
 export const BookRoutes = router
