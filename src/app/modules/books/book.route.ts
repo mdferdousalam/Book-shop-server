de613178dd1a9b17bede14b793711c5d0aaba476
mdferdousalam <mdferdousalam1989@yahoo.com>
2023-07-27 10:42:05 +0600

    book route updated

--- src/app/modules/books/book.route.ts
+++ src/app/modules/books/book.route.ts
@@ -3,7 +3,7 @@ import { createBookHandler, deleteBookHandler, getAllBooksHandler, getBookByIdHa
 const router = express.Router()
 
 router.get('/all-books', getAllBooksHandler)
-router.get('/single-book:id', getBookByIdHandler)
+router.get('/single-book/:id', getBookByIdHandler)
 router.post('/add-book', createBookHandler)
 router.patch('/edit-book/:id', updateBookHandler)
 router.delete('/delete-book/:id', deleteBookHandler)
