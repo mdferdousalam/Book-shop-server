189a519720e045783700272edf26e819b872350d
mdferdousalam <mdferdousalam1989@yahoo.com>
2023-07-27 10:20:36 +0600

    book route changed

--- src/app/modules/books/book.route.ts
+++ src/app/modules/books/book.route.ts
@@ -2,8 +2,8 @@ import express from 'express'
 import { createBookHandler, deleteBookHandler, getAllBooksHandler, getBookByIdHandler, updateBookHandler } from './book.controller'
 const router = express.Router()
 
-router.get('/', getAllBooksHandler)
-router.get('/:id', getBookByIdHandler)
+router.get('/all-books', getAllBooksHandler)
+router.get('/single-book:id', getBookByIdHandler)
 router.post('/add-book', createBookHandler)
 router.patch('/edit-book/:id', updateBookHandler)
 router.delete('/delete-book/:id', deleteBookHandler)
