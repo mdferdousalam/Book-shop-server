2e70bcbaad46ccc21de76618839f479720ba4dbc
mdferdousalam <mdferdousalam1989@yahoo.com>
2023-12-31 09:25:50 +0600

    routes

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,12 @@ import express, { Application, NextFunction, Request, Response } from 'express'
 import httpStatus from 'http-status'
 import globalErrorHandler from './app/middlewares/globalErrorHandler'
 import routes from './app/routes'
-import { UserRoutes } from './app/modules/user/user.route'
-import { AdminRoutes } from './app/modules/admin/admin.route'
-import { AuthRoutes } from './app/modules/auth/auth.route'
-import { BookRoutes } from './app/modules/books/book.route'
-import { ReviewRoutes } from './app/modules/reviews/review.route'
-import { WishlistRoutes } from './app/modules/wishlist/wishlist.route'
+// import { UserRoutes } from './app/modules/user/user.route'
+// import { AdminRoutes } from './app/modules/admin/admin.route'
+// import { AuthRoutes } from './app/modules/auth/auth.route'
+// import { BookRoutes } from './app/modules/books/book.route'
+// import { ReviewRoutes } from './app/modules/reviews/review.route'
+// import { WishlistRoutes } from './app/modules/wishlist/wishlist.route'
 const app: Application = express()
 app.use(cors())
 
@@ -20,12 +20,12 @@ import cookieParser from 'cookie-parser'
 
 app.use(cookieParser())
 
-app.use('/api/v1/users/', UserRoutes)
-app.use('/api/v1/admin/', AdminRoutes)
-app.use('/api/v1/auth/', AuthRoutes)
-app.use('/api/v1/books/', BookRoutes)
-app.use('/api/v1/review/', ReviewRoutes)
-app.use('/api/v1/wishlist/', WishlistRoutes)
+// app.use('/users/', UserRoutes)
+// app.use('/admin/', AdminRoutes)
+// app.use('/api/v1/auth/', AuthRoutes)
+// app.use('/api/v1/books/', BookRoutes)
+// app.use('/api/v1/review/', ReviewRoutes)
+// app.use('/api/v1/wishlist/', WishlistRoutes)
 
 app.use('/api/v1', routes)
 
